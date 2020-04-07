a58dfaf0e73c6b19715a82d8a14fa708d04fdd08
aemarquina <aeamarquina@gmail.com>
2020-04-07 15:01:59 -0400

    Changes made to the production url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://rhubarb-custard-94583.herokuapp.com/',
+  production: 'https://rhubarb-custard-94583.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
